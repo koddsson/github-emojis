6bfc02fffb81a6c3fecd6fefbe770339fe023d92
Kristján Oddsson <koddsson@gmail.com>
2014-09-07 21:51:33 +0000

    Finish off the extension.

--- script.js
+++ script.js
@@ -6,17 +6,20 @@ if (typeof String.prototype.startsWith != 'function') {
 
 var findEmoji = function(text, callback) {
     $.getJSON(chrome.extension.getURL('emojis.json'), function(emojis) {
-        var results = _(emojis).chain()
-                               .pairs()
-                               .filter(function(emoji) {
-                                   var in_list = _(emoji[1]).reduce(function (last, desc) {
-                                      return last || desc.startsWith(text);
-                                   }, false);
-                                   if (emoji[0].startsWith(text) || in_list) {
-                                       return emoji;
-                                   }
-                               })
-                               .value();
+        var results = _(emojis)
+                        .chain()
+                        .pairs()
+                        .filter(function(emoji) {
+                            // Search the list of descriptions for the given string
+                            var in_list = _(emoji[1]).reduce(function (last, desc) {
+                              return last || desc.startsWith(text);
+                            }, false);
+                            if (emoji[0].startsWith(text) || in_list) {
+                               return emoji[0];
+                            }
+                        })
+                        .map(_.first)
+                        .value();
         callback(results);
     });
 };
@@ -25,23 +28,17 @@ $(document).ready(function() {
     var searchEmojis = false;
     var searchTerm = '';
 
-    $('textarea#new_comment_field').keypress(function(event) {
-        if (searchEmojis) {
-            searchTerm += String.fromCharCode(event.which);
-            console.log(searchTerm);
-            findEmoji(searchTerm, function(data) {
-                console.log(data);
-            });
+    $('textarea#new_comment_field').textcomplete([
+        {
+            match: /(^|\s)\$(\w*)$/,
+            replace: function (value) { return '$1:' + value + ':'; },
+            template: function (value) {
+                var imgURL = chrome.extension.getURL("bower_components/jquery-textcomplete/media/images/emoji/" + value + ".png");
+                return '<img src="' + imgURL + '"></img>' + value;
+            },
+            search:    function (term, callback) {
+                findEmoji(term, callback);
+            }
         }
-        if (event.which === 36 ) { // 36 === $
-            console.log('Started searching for emojis!');
-            searchEmojis = true;
-        }
-    }).keyup(function (event) {
-        // We need the keyup event handler just for catching esc
-        if (event.keyCode === 27) {
-            searchEmojis = false;
-            searchTerm = '';
-        }
-    });
+    ]);
 });
