8317c8e6b219f1243f8a700060afbe7a5c27cd0f
Kristján Oddsson <koddsson@gmail.com>
2014-09-09 15:21:34 +0000

    Code cleanup!

--- script.js
+++ script.js
@@ -7,41 +7,40 @@ if (typeof String.prototype.startsWith != 'function') {
 var findEmoji = function(text, callback) {
     $.getJSON(chrome.extension.getURL('emojis.json'), function(emojis) {
         var results = _(emojis)
-                        .chain()
-                        .pairs()
-                        .filter(function(emoji) {
-                            // Search the list of descriptions for the given string
-                            var in_list = _(emoji[1]).reduce(function (last, desc) {
-                              return last || desc.startsWith(text);
-                            }, false);
-                            if (emoji[0].startsWith(text) || in_list) {
-                               return emoji[0];
-                            }
-                        })
-                        .map(_.first)
-                        .value();
+          .chain()
+          .pairs()
+          .filter(function(emoji) {
+              // Search the list of descriptions for the given string
+              var in_list = _(emoji[1]).reduce(function (last, desc) {
+                return last || desc.startsWith(text);
+              }, false);
+              if (emoji[0].startsWith(text) || in_list) {
+                 return emoji[0];
+              }
+          })
+          .map(_.first)
+          .value();
         callback(results);
     });
 };
 
-var strats = [
-    {
-        match: /(^|\s)\$(\w*)$/,
-        replace: function (value) { return '$1:' + value + ':'; },
-        template: function (value) {
-            var imgURL = chrome.extension.getURL("bower_components/jquery-textcomplete/media/images/emoji/" + value + ".png");
-            return '<img src="' + imgURL + '"></img>' + value;
-        },
-        search:    function (term, callback) {
-            findEmoji(term, callback);
-        },
-        maxCount: 5
-    }
-];
 
 $(document).ready(function() {
     // Oh god this is bad.
     setInterval(function() {
-        $('textarea.comment-form-textarea').textcomplete(strats);
+        $('textarea.comment-form-textarea').textcomplete([
+            {
+                match: /(^|\s)\$(\w*)$/,
+                replace: function (value) { return '$1:' + value + ':'; },
+                template: function (value) {
+                    var imgURL = chrome.extension.getURL("bower_components/jquery-textcomplete/media/images/emoji/" + value + ".png");
+                    return '<img src="' + imgURL + '"></img>' + value;
+                },
+                search:    function (term, callback) {
+                    findEmoji(term, callback);
+                },
+                maxCount: 5
+            }
+        ])
     }, 1000);
 });
